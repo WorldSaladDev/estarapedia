a5190f981f63c9dea6ecb2cc0eb1aebe4f38b292
RadioctiveTaco <worldsaladdev@gmail.com>
2024-07-22 15:33:54 -1000

    Commit 5 7/22/2024
    changed image formatting and edited articles

--- js/estarapedia_home.js
+++ js/estarapedia_home.js
@@ -4,18 +4,14 @@ window.onload = function() {
         element.style.opacity = 0;
     });
 
-    var fadeInImage = document.getElementById('fade-in-1');
+    var fadeInImage = document.getElementById('fade-in');
     setTimeout(function() {
         fadeInImage.style.opacity = 1;
 
-        var fadeInText = document.getElementById('fade-in-2');
+        var fadeInText = document.getElementById('fade-in-1');
         setTimeout(function() {
             fadeInText.style.opacity = 1;
-            
-            var fadeInElement3 = document.getElementById('fade-in-3');
-            setTimeout(function() {
-                fadeInElement3.style.opacity = 1;
-            }, 300); // Adjust this value to match the duration of the text fading in
-        }, 300); // Adjust this value to match the duration of the image fading in
+        
+        }, 2); // Adjust this value to match the duration of the image fading in
     }, 0); // No delay for the image to appear
 }
